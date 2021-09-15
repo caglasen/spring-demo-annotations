f13cb4d92b730bceb6fe8218eed9fd72bed1d6a4
Cagla Sen <cagla1876@hotmail.com>
2021-09-15 09:52:22 +0300

    74. FAQ: How to inject properties file using Java annotations

---
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SwimCoach implements Coach {
+	
+	@Value("$(foo.email)")
+	private String email;
+	
+	@Value("$(foo.team)")
+	private String team;
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
